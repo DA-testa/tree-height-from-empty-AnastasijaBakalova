0906af3597326fd42cb8bf5b97a4fe7e19923e08
Bakalova Anastasija <125196954+AnastasijaBakalova@users.noreply.github.com>
2023-02-28 15:45:19 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -3,31 +3,48 @@
 import sys
 import threading
 import numpy
+list = []
+flags = []
+countRoute = []
 
-
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
+def compute_height(n, parents, parent_children):
+  #print(parent_children)
+  if (flags[n]!=1):
+    flags[n]=1;
+    if (parents == -1):
+      max_height = 1;
+      countRoute[n] = 1;
+      
+    else :
+      max_height = 1 + compute_height(parents, parent_children[int(parents)], parent_children)
+      countRoute[n] = max_height
     return max_height
+  else :
+    return countRoute[n]
 
 
 def main():
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+  readfrom = input()
+  if ("I" in readfrom) or ("i" in readfrom) :
+    count = int (input())
+    parent_children = [int(j) for j in input().split()]
+    #print(parent_children[4])
+    for i in range(0, count, 1) :
+      list.append(i)
+      flags.append(0)
+      countRoute.append(0)
+  if ("F" in readfrom) or ("f" in readfrom):
     pass
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+
+
+  max = 0
+  for i in range (0, count, 1):
+    max2 = compute_height(i,parent_children[i], parent_children)
+    if(max2>max):
+      max= max2
+  print(max)
 main()
-# print(numpy.array([1,2,3]))
